16a6ed727bbb6b3388768878c14ba11323a4c642
Adam Scharf <adam.scharf@studsvik.com>
2024-07-26 12:58:13 -0500

    New method for db update that splits league list and sends to queue

---
+++ src/lambda_functions/ottoneu-db-update/lambda_function.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+import os
+
+sqs = boto3.client('sqs')
+lambda_client = boto3.client('lambda')
+queueurl = sqs.get_queue_url(QueueName='Ottoneu-db-update-queue')['QueueUrl']
+
+def lambda_handler(event, context):
+    
+    league_id_str = os.environ['league_ids_list']
+    
+    print(league_id_str)
+    
+    league_ids = json.loads(league_id_str)
+    
+    league_id_chunks = divide_lists(league_ids, int(os.environ['list_chunk_size']))
+    
+    for id_chunk in league_id_chunks:
+        print(f'length = {len(id_chunk)}')
+        print(id_chunk)
+        msg_map = dict()
+        msg_map['league_ids'] = id_chunk
+    
+        try:
+            sqs.send_message(QueueUrl=queueurl, MessageBody=json.dumps(msg_map))
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400, 
+                'body': json.dumps('Error when submitting to the queue.')
+            }
+    
+    response = lambda_client.invoke(
+        FunctionName = os.environ['player_put_arn'],
+        InvocationType = 'RequestResponse'
+        )
+        
+    print(response)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Update success')
+    }
+
+def divide_lists(l, n): 
+      
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
--- src/lambda_functions/ottoneu-save-rosters/lambda_function.py
+++ src/lambda_functions/ottoneu-save-rosters/lambda_function.py
@@ -9,16 +9,30 @@ ottoneu_db = client.ottoneu
 lambda_client = boto3.client('lambda')
 
 def lambda_handler(event, context):
+    print(event)
+    
+    if event.get('Records', None):
+        msg_map = json.loads(event['Records'][0]['body'])
+    else:
+        msg_map = event
+    
+    print(msg_map)
+    
+    if not msg_map.get('league_ids', None):
+        return {
+            'statusCode': 400,
+            'body': json.dumps('league_ids not present in event')
+        }
+    league_ids = msg_map['league_ids']
+    
     leagues_col = ottoneu_db.leagues
 
     update_leagues = list()
 
-    #TODO: Get league table
-    league_ids = ['160']
-
     roster_load_version = os.environ['roster_lambda_version']
 
     for league_id in league_ids:
+        print(f'Getting league_id: {league_id}' )
         try:
             roster_load_parameters = {
                 "league_id" : league_id
@@ -33,13 +47,19 @@ def lambda_handler(event, context):
             
             lambda_response = json.load(response['Payload'])
             
+            print(response)
+            
             if 'body' in lambda_response:
                 league_dict = json.loads(lambda_response['body'])
             else:
                 league_dict = None
                 
             if not league_dict:
+                print(f'!!League_id {league_id} not valid')
                 continue
+            
+            print(f'Roster length {len(league_dict)}')
+            
             update_leagues.append(UpdateOne({'_id': league_id},  {'$set': league_dict}, upsert=True))
         except Exception as e:
             print(e)
@@ -57,6 +77,3 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200
     }
-
-def get_league_ids():
-    ...

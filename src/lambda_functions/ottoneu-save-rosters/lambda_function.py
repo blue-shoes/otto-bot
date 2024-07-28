21194294262d02dbb49786df99848ca1e62ef5fd
Adam Scharf <adam.scharf@studsvik.com>
2024-07-28 18:44:03 -0500

    Rework ottoneu-save-rosters to not call additional lambdas to avoid throttling

--- src/lambda_functions/ottoneu-save-rosters/lambda_function.py
+++ src/lambda_functions/ottoneu-save-rosters/lambda_function.py
@@ -2,6 +2,10 @@ import json
 from pymongo import MongoClient, UpdateOne
 import os
 import boto3
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as Soup
+from io import StringIO
 
 client = MongoClient(host=os.environ.get("ATLAS_URI"))
 ottoneu_db = client.ottoneu
@@ -9,6 +13,8 @@ ottoneu_db = client.ottoneu
 lambda_client = boto3.client('lambda')
 
 def lambda_handler(event, context):
+    # TODO implement
+
     print(event)
     
     if event.get('Records', None):
@@ -29,39 +35,26 @@ def lambda_handler(event, context):
 
     update_leagues = list()
 
-    roster_load_version = os.environ['roster_lambda_version']
+    print(f'All leagues: {league_ids.length}')
+
+    #league_ids = ['1', '3', '13', '15', '16', '26', '28', '32']
+
+    print(f'League subset: {len(league_ids)}')
 
     for league_id in league_ids:
-        print(f'Getting league_id: {league_id}' )
+        #print(f'Getting league_id: {league_id}' )
         try:
-            roster_load_parameters = {
-                "league_id" : league_id
-            }
-            
-            response = lambda_client.invoke(
-                FunctionName = os.environ['roster_lambda_arn'],
-                InvocationType = 'RequestResponse',
-                Payload = json.dumps(roster_load_parameters),
-                Qualifier = roster_load_version
-            )
-            
-            lambda_response = json.load(response['Payload'])
-            
-            print(response)
-            
-            if 'body' in lambda_response:
-                league_dict = json.loads(lambda_response['body'])
-            else:
-                league_dict = None
+            league_dict = get_league_dict(league_id)
                 
             if not league_dict:
                 print(f'!!League_id {league_id} not valid')
                 continue
             
-            print(f'Roster length {len(league_dict)}')
+            #print(f'Roster length {len(league_dict)}')
             
             update_leagues.append(UpdateOne({'_id': league_id},  {'$set': league_dict}, upsert=True))
         except Exception as e:
+            print(f'Exception for league {league_id}')
             print(e)
 
     if update_leagues:
@@ -77,3 +70,17 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200
     }
+
+def get_league_dict(lg_id: str):
+    '''Scrapes the /rosterexport page for a league (in csv format) and returns a DataFrame of the information. Index is Ottoneu Id'''
+    if lg_id:
+        roster_export_url = f'https://ottoneu.fangraphs.com/{lg_id}/rosterexport'
+        response = requests.get(roster_export_url)
+        
+        rost_soup = Soup(response.text, 'html.parser')
+        df = pd.read_csv(StringIO(rost_soup.contents[0]))
+        df.set_index("ottoneu ID", inplace=True)
+        df.index = df.index.astype(str, copy = False)
+        return df.to_dict('index')
+    
+    return dict()
